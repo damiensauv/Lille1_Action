68cc70dd22224a1185941c025b3f35a9a0cbf2ff
damiensauv <damiensauvalle@gmail.com>
2015-11-04 12:38:35 +0100

    comment

--- ActionPiscine/src/pool/Swimmer.java
+++ ActionPiscine/src/pool/Swimmer.java
@@ -2,12 +2,20 @@ package pool;
 
 import actions.Action;
 
-
+// extend action ?
 public class Swimmer implements Action
 {
 	public Swimmer(String name, RessourcePool<Basket> baskets, RessourcePool<Cubicle> cubicles, int undress, int swim, int dress)
 	{
-		
+		// take basket
+		// taket Cubicle
+		// undress
+		// free Cubicle
+		// swim
+		// re-take Cubicle
+		// dress
+		// give back cubicle
+		// give back basket
 	}
 
 }
