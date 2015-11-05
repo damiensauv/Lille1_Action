e016c80a15596bd8126cae527970f0d40073dc98
Sauvalle Damien <sauvalle@a11p12.fil.univ-lille1.fr>
2015-11-05 16:49:10 +0100

    add Take & Free Resource

---
+++ ActionPiscine/src/pool/FreeResourceAction.java
@@ -0,0 +1,32 @@
+package pool;
+
+import actions.*;
+
+public class FreeResourceAction extends Action
+{
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInProgress() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected Action createAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
