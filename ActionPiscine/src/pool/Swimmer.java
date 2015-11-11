730f95f02fd047456425664289992c9fc2955b9b
damiensauv <damiensauvalle@gmail.com>
2015-11-11 22:33:51 +0100

    ok

--- ActionPiscine/src/pool/Swimmer.java
+++ ActionPiscine/src/pool/Swimmer.java
@@ -21,9 +21,9 @@ public class Swimmer extends Action
         this.cubicleUser = new ResourcefulUser<Cubicle>(name);
 
         this.process.addAction(new TakeResourceAction<Basket>(baskets, basketUser));//prend un panier                                                                                                                          
-
         this.process.addAction(new TakeResourceAction<Cubicle>(cubicles, cubicleUser));// prend une cabine                                                                                                                     
-/* 
+
+        /* 
         this.process.addAction(new UndressingAction(undress)); // se déshabille                                                                                                              
         this.process.addAction(new FreeResourceAction<Cubicle>(cubicles, cubicleUser)); // libère la cabine                                                                                                                    
         this.process.addAction(new SwimmingAction(swim));                      // nage                                                                                                                                   
